c0c0ce2e0727b13f2f426d607fc59428c585be69
rodrigomgrana <rodrigograna@live.com.ar>
2022-06-17 16:12:58 -0300

    17/06

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
-const express = require("express")
+const express = require("express");
+const path = require("path");
 
-const path = require("path")
+const app = express();
+//const PORT = process.env.PORT || 3040;
 
-const app = express()
+app.use(express.static("public"));
 
-app.use ("/static", express.static(__dirname + "/public"))
+app.get("/", (req, res) => {
+  let indexPath = path.join(__dirname, "./views/home.html");
+  res.sendFile(indexPath);
+});
+app.get("/register", (req, res) => {
+  let indexPath = path.join(__dirname, "./views/register.html");
+  res.sendFile(indexPath);
+});
+app.get("/login", (req, res) => {
+  let indexPath = path.join(__dirname, "./views/login.html");
+  res.sendFile(indexPath);
+});
 
-
-app.listen (3001, ()=> { console.log("servidor escuchando puerto 3001")})
-
-app.get ("/", (req,res) =>{
- res.sendFile(__dirname + "/views/home.html")
-})
+ app.listen(3040, () => console.log("ingresó correctamente")); 
+//app.listen(PORT, () => console.log("running on port 3040"));
